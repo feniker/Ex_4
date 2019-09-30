d6b762030dd878280979e9e50fdaa47282c1800b
Константин Колесниченко <kolesnichenko.00@mail.ru>
2019-09-30 14:46:40 +0700

    Add q

--- Ex_4/Main.cpp
+++ Ex_4/Main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
 	vector <Figure*> vec;
+	int q = 0;
 	Circle cir(0, 0, 0, 1);
 	Triangle tri({ 0,0 }, { 1,0 }, { 0,1 });
 	Quadrangle qua({ 3,0 }, { 0,3 }, { 0,0 }, { 3,3 });
@@ -12,7 +13,7 @@ int main()
 	vec = { &cir,
 	&tri,
 	&qua,
-	&oth };
+	&oth};
 	for (int i = 0; i < vec.size(); i++)
 	{
 		vec[i]->print();
